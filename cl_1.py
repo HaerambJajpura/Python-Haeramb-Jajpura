64cff95ea86b6d3ee75210206694a4a2b48923e6
HaerambJajpura <haerambjajpura1892006@gmail.com>
2024-08-05 11:35:59 +0530

    cl 1 updated and cl 2 upoaded

---
+++ cl_1.py
@@ -0,0 +1,11 @@
+print("Hello world")
+# print function is used to print things in python
+print("I am Haeramb Jajpura")
+print("I am a student")
+print("I am 17 years old")
+print("I live in Jaipur")
+print("i speak Hindi, English and some more languages ")
+print("my hobbys are programming, robotics, reading books, hearing music etc")
+
+
+# python automatically changes the line and we dont need to tell it to change line
---
+++ cl_2.py
@@ -0,0 +1,52 @@
+# this is a single line comment
+
+print("Subscribe My channel Now")
+print("Also like the video right now")
+# python automatically changes the line
+# please dont remove this line from the code
+
+
+
+# if you don't want to change the line then do this
+print("This is line 1",end="")
+print("this is line 2")
+
+
+print("This is line 1",end=" ")
+print("this is line 2")
+
+
+
+print("This is line 1",end="::::::new line::::")
+print("this is line 2")
+
+# by deafult end is a new line character
+
+
+print("This is line one","this is line 2",end="\n")
+# yaha par beech me , laga diya to space aa jaegi
+
+
+print("C:\nenovo")
+
+# then how to print it
+print("c:\\nenovo")
+
+
+# how to print " and '
+print("\"")
+print("\'")
+
+
+
+# some escape sequence characters
+print("first line\nsecond line")
+# \n for new line
+# \t for tab space
+'''
+this
+is
+a
+multiline
+comment
+'''
